572bad14b35d8544ea25fe489f8a4b560482df6e
EsraOzturk <esosh77@hotmail.com>
2019-12-24 20:10:10 -0500

    Selenium

---
+++ src/Day9/Homework.java
@@ -0,0 +1,7 @@
+package Day9;
+
+public class Homework {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Day9/ModelWindowClickProblem.java
@@ -0,0 +1,12 @@
+package Day9;
+
+public class ModelWindowClickProblem {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
